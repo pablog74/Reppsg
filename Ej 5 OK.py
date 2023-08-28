8de8fabc2fc4f828cd679c2afcae18919cf6df34
PABLO GARIDO <MT23782030@osdebinario.grupoosde>
2023-08-27 23:55:45 -0300

    ej 5 sin SUM

---
+++ Ej 5 OK.py
@@ -0,0 +1,13 @@
+#sumar elementos de una lista con metodo sum
+
+#listan = [1, 4, 5,10]
+#print (sum(listan))
+
+#sumar elementos de una lista SIN sum
+
+listan = [1, 4, 5, 10]
+suma = 0
+for x in listan:
+    suma = suma + x
+print (suma)
+
