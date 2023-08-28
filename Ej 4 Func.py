fde0ffcd69c19c038396599b0045b625b4e0570a
PABLO GARIDO <MT23782030@osdebinario.grupoosde>
2023-08-28 19:03:46 -0300

    subo ej 4 de funciones

---
+++ Ej 4 Func.py
@@ -0,0 +1,12 @@
+def intermedio (a, b):
+
+    prom = (a+b)/2
+
+    return prom 
+
+p1=float(input("Ingrese p1 "))
+p2=float(input("Ingrese p2 "))
+
+resultado=intermedio (p1, p2)
+
+print (resultado)
